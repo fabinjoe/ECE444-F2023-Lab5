3687539d7d9364499816cf5dae6e3c7618559f3a
Fabin Joe <fabin2005@outlook.com>
2023-10-29 12:07:04 -0400

    Linting and Code Formatting

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,4 +1,3 @@
-import os
 import pytest
 from pathlib import Path
 import json
@@ -6,6 +5,7 @@ import json
 from project.app import app, db
 
 TEST_DB = "test.db"
+
 
 @pytest.fixture
 def client():
@@ -19,6 +19,7 @@ def client():
         yield app.test_client()  # tests run here
         db.drop_all()  # teardown
 
+
 def login(client, username, password):
     """Login helper function"""
     return client.post(
@@ -27,23 +28,28 @@ def login(client, username, password):
         follow_redirects=True,
     )
 
+
 def logout(client):
     """Logout helper function"""
     return client.get("/logout", follow_redirects=True)
 
+
 def test_index(client):
     response = client.get("/", content_type="html/text")
     assert response.status_code == 200
+
 
 def test_database(client):
     """initial test. ensure that the database exists"""
     tester = Path("test.db").is_file()
     assert tester
 
+
 def test_empty_db(client):
     """Ensure database is blank"""
     rv = client.get("/")
     assert b"No entries yet. Add some!" in rv.data
+
 
 def test_login_logout(client):
     """Test login and logout using helper functions"""
@@ -55,6 +61,7 @@ def test_login_logout(client):
     assert b"Invalid username" in rv.data
     rv = login(client, app.config["USERNAME"], app.config["PASSWORD"] + "x")
     assert b"Invalid password" in rv.data
+
 
 def test_messages(client):
     """Ensure that user can post messages"""
@@ -68,6 +75,7 @@ def test_messages(client):
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
 
+
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
     rv = client.get("/delete/1")
@@ -77,4 +85,3 @@ def test_delete_message(client):
     rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
-
