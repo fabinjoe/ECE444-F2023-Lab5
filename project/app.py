4af3df9649c347916df012614902fbba23947f27
Fabin Joe <fabin2005@outlook.com>
2023-10-29 12:03:50 -0400

    Adding Search functionalities

--- project/app.py
+++ project/app.py
@@ -1,5 +1,6 @@
 import sqlite3
 from pathlib import Path
+from functools import wraps
 
 from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
@@ -63,7 +64,17 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('index'))
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            flash('Please log in.')
+            return jsonify({'status': 0, 'message': 'Please log in.'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route('/delete/<int:post_id>', methods=['GET'])
+@login_required
 def delete_entry(post_id):
     """Deletes post from database."""
     result = {'status': 0, 'message': 'Error'}
@@ -75,6 +86,14 @@ def delete_entry(post_id):
     except Exception as e:
         result = {'status': 0, 'message': repr(e)}
     return jsonify(result)
+
+@app.route('/search/', methods=['GET'])
+def search():
+    query = request.args.get("query")
+    entries = db.session.query(models.Post)
+    if query:
+        return render_template('search.html', entries=entries, query=query)
+    return render_template('search.html')
 
 if __name__ == "__main__":
     app.run()
